788d35dd2fc29b666690952e2da90dcd6d21be2b
av4sin <gmondb00@estudiantes.unileon.es>
2025-11-07 19:06:28 +0100

    Cambio el fallo detectado en el taller

--- src/pressToLed.py
+++ src/pressToLed.py
@@ -4,7 +4,7 @@ from time import sleep
 led = LED(17)
 button = Button(2)
 
-while true:
+while True: # En python los booleanos van en mayuscula la primera
   button.wait_for_press()
   led.on()
   sleep(3)
